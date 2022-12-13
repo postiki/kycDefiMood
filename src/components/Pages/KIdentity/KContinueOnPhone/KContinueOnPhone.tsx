d7960a23384cb5ff9336711859112dbacc45a756
postiki <egorsheleznov@gmail.com>
2022-12-13 22:31:49 +0400

    update

--- src/components/Pages/KIdentity/KContinueOnPhone/KContinueOnPhone.tsx
+++ src/components/Pages/KIdentity/KContinueOnPhone/KContinueOnPhone.tsx
@@ -25,7 +25,7 @@ const KContinueOnPhone: React.FC<IKContinueOnPhoneProps> = () => {
             expiresIn: Math.floor(Date.now()) + Number(process.env.REACT_APP_TOKEN_EXP)//1 minute
         }, process.env.REACT_APP_SECRECT_TOKEN || '');
 
-        return setUrl(`${process.env.REACT_APP_API_ENDPOINT}/?token=${token}`)
+        return setUrl(`${process.env.REACT_APP_API_QR}/?token=${token}`)
     }
 
     useEffect(() => {
